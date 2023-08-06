7f5e3ac4f2e3536b6895f6fe3a4f36cc0e6103c7
unknown <jomanugo@gmail.com>
2023-08-06 21:00:07 +0200

    tabla vista recetas

--- resources/views/recetas.blade.php
+++ resources/views/recetas.blade.php
@@ -5,13 +5,33 @@
 @endsection
 
 @section("cuerpo")
+<table class="table table-hover">
+  <thead>
+    <tr>
+      <th scope="col">Nombre</th>
+      <th scope="col">Ingredientes</th>
+      <th scope="col">Elaboración</th>
+    </tr>
+  </thead>
+  <tbody>
+
 <?php
 
 use App\Models\Receta;
- 
+
 foreach (Receta::all() as $receta) {
-    echo "<p>Nombre: " . $receta->nombrereceta ."<br>Ingredientes: " . $receta->ingredientes .  "<br>Elaboración: " . $receta->elaboracion . "</p>";
+  
+    echo '<tr>';
+            echo '<td>'. $receta->nombrereceta .'</td>';
+            echo '<td>'. $receta->ingredientes .'</td>';
+            echo '<td>'. $receta->elaboracion .'</td>';
+    echo '</tr>';
+   
+    //echo "<p>Nombre: " . $receta->nombrereceta ."<br>Ingredientes: " . $receta->ingredientes .  "<br>Elaboración: " . $receta->elaboracion . "</p>";
 }
     
 ?>
+
+</tbody>
+</table>
 @endsection
